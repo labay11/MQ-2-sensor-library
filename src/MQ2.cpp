cff1c99929e9c3b874485b982723dabe33ee244b
labay11 <adria.labay@gmail.com>
2021-02-20 19:36:54 +0100

    simplify read procedure and return value from read all

--- src/MQ2.cpp
+++ src/MQ2.cpp
@@ -3,7 +3,9 @@
 
 MQ2::MQ2(int pin) {
   _pin = pin;
+  Ro = -1.0;
 }
+
 void MQ2::begin(){
     Ro = MQCalibration();
     Serial.print("Ro: ");
@@ -11,53 +13,70 @@ void MQ2::begin(){
     Serial.println(" kohm");
 }
 
-float* MQ2::read(bool print){
+void MQ2::close(){
+	Ro = -1.0;
+	values[0] = 0.0;
+	values[1] = 0.0;
+	values[2] = 0.0;
+}
 
-   lpg = MQGetGasPercentage(MQRead()/Ro,GAS_LPG);
-   co = MQGetGasPercentage(MQRead()/Ro,GAS_CO);
-   smoke = MQGetGasPercentage(MQRead()/Ro,GAS_SMOKE);
+bool MQ2::checkCalibration() {
+	if (Ro < 0.0) {
+		Serial.println("Device not calibrated, call MQ2::begin before reading any value.");
+		return false;
+	}
+
+	return true;
+}
+
+float* MQ2::read(bool print){
+	if (!checkCalibration()) return NULL;
+
+   values[0] = MQGetPercentage(LPGCurve);
+   values[1] = MQGetPercentage(COCurve);
+   values[2] = MQGetPercentage(SmokeCurve);
+
+   lastReadTime = millis();
 
    if (print){
        Serial.print("LPG:");
-       Serial.print(lpg);
-       Serial.print( "ppm" );
-       Serial.print("    ");
+       Serial.print(values[0]);
+       Serial.print("ppm\t");
        Serial.print("CO:");
-       Serial.print(co);
-       Serial.print( "ppm" );
-       Serial.print("    ");
+       Serial.print(values[1]);
+       Serial.print("ppm\t");
        Serial.print("SMOKE:");
-       Serial.print(smoke);
-       Serial.print( "ppm" );
-       Serial.print("\n");
+       Serial.print(values[2]);
+       Serial.print("ppm\n");
    }
-   lastReadTime = millis();
-   static float values[3] = {lpg,co,smoke};
    return values;
 }
 
 float MQ2::readLPG(){
-    if (millis()<(lastReadTime + 10000) && lpg != 0){
-        return lpg;
-    }else{
-        return lpg = MQGetGasPercentage(MQRead()/10,GAS_LPG);
-    }
+	if (!checkCalibration()) return 0.0;
+
+    if (millis()<(lastReadTime + 10000) && values[0] > 0)
+        return values[0];
+    else
+        return (values[0] = MQGetPercentage(LPGCurve));
 }
 
 float MQ2::readCO(){
-    if (millis()<(lastReadTime + 10000) && co != 0){
-        return co;
-    }else{
-        return co = MQGetGasPercentage(MQRead()/10,GAS_CO);
-    }
+	if (!checkCalibration()) return 0.0;
+
+    if (millis()<(lastReadTime + 10000) && values[1] > 0)
+        return values[1];
+    else
+        return (values[1] = MQGetPercentage(COCurve));
 }
 
 float MQ2::readSmoke(){
-    if (millis()<(lastReadTime + 10000) && smoke != 0){
-        return smoke;
-    }else{
-        return smoke = MQGetGasPercentage(MQRead()/10,GAS_SMOKE);
-    }
+	if (!checkCalibration()) return 0.0;
+
+    if (millis()<(lastReadTime + 10000) && values[2] > 0)
+        return values[2];
+    else
+        return (values[2] = MQGetPercentage(SmokeCurve));
 }
 
 float MQ2::MQResistanceCalculation(int raw_adc) {
@@ -65,41 +84,34 @@ float MQ2::MQResistanceCalculation(int raw_adc) {
 }
 
 float MQ2::MQCalibration() {
-  float val=0;
- 
-  for (int i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
-    val += MQResistanceCalculation(analogRead(_pin));
-    delay(CALIBRATION_SAMPLE_INTERVAL);
-  }
-  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
- 
-  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
-                                                        //according to the chart in the datasheet 
-  return val; 
-}
-float MQ2::MQRead() {
-  int i;
-  float rs=0;
-  int val = analogRead(_pin);
+	float val=0.0;
 
-  for (i=0;i<READ_SAMPLE_TIMES;i++) {
-    rs += MQResistanceCalculation(val);
-    delay(READ_SAMPLE_INTERVAL);
-  }
- 
-  rs = rs/READ_SAMPLE_TIMES;
-  return rs;  
+	for (int i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+		val += MQResistanceCalculation(analogRead(_pin));
+		delay(CALIBRATION_SAMPLE_INTERVAL);
+	}
+	val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+
+	val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+	                                                    //according to the chart in the datasheet 
+	return val; 
 }
-float MQ2::MQGetGasPercentage(float rs_ro_ratio, int gas_id) {
-  if ( gas_id == GAS_LPG ) {
-     return MQGetPercentage(rs_ro_ratio,LPGCurve);
-  } else if ( gas_id == GAS_CO ) {
-     return MQGetPercentage(rs_ro_ratio,COCurve);
-  } else if ( gas_id == GAS_SMOKE ) {
-     return MQGetPercentage(rs_ro_ratio,SmokeCurve);
-  }    
-  return 0;
+
+float MQ2::MQRead() {
+	int i;
+	float rs=0.0;
+	int val = analogRead(_pin);
+
+	for (i=0;i<READ_SAMPLE_TIMES;i++) {
+		rs += MQResistanceCalculation(val);
+		delay(READ_SAMPLE_INTERVAL);
+	}
+
+	rs = rs/READ_SAMPLE_TIMES;
+	return rs;  
 }
-int MQ2::MQGetPercentage(float rs_ro_ratio, float *pcurve) {
-  return (pow(10,(((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+
+int MQ2::MQGetPercentage(float *pcurve) {
+	float rs_ro_ratio = MQRead() / Ro;
+	return (pow(10.0,(((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
 }
--- src/MQ2.h
+++ src/MQ2.h
@@ -10,42 +10,65 @@
 #endif
 
 class MQ2 {
-public: 
-	MQ2(int pin);
-	float* read(bool print);
-	float readLPG();
-	float readCO();
-	float readSmoke();
-	void begin();
-private:
-	int _pin;
-	int RL_VALUE = 5;     //define the load resistance on the board, in kilo ohms
-	int RO_CLEAN_AIR_FACTOR = 9.83;  
-	int CALIBARAION_SAMPLE_TIMES = 5; 
-	int CALIBRATION_SAMPLE_INTERVAL = 50;
-	int READ_SAMPLE_INTERVAL = 50;
-	int READ_SAMPLE_TIMES = 5;
+	public: 
+		/*
+		 * Creates the sensor by giving the pin where it is attached.
+		 */
+		MQ2(int pin);
 
-	float LPGCurve[3]  =  {2.3,0.21,-0.47}; 
-	float COCurve[3]  =  {2.3,0.72,-0.34};   
-	float SmokeCurve[3] = {2.3,0.53,-0.44};                                                       
-	float Ro = 10;             
- 
-    	int GAS_LPG = 0;
-	int GAS_CO = 1;
-	int GAS_SMOKE = 2;
+		/*
+		 * Initialises the sensor before getting any data.
+		 * This step is necessary as it needs to be calibrated based on
+		 * current air and temperature conditions.
+		 */
+		void begin();
 
-	float lpg = 0;
-	float co = 0;
-	float smoke = 0;
-	
-	float MQRead();
-	float MQGetGasPercentage(float rs_ro_ratio, int gas_id);
-	int MQGetPercentage(float rs_ro_ratio, float *pcurve);
-	float MQCalibration();
-	float MQResistanceCalculation(int raw_adc);
+		/*
+		 * Stops the sensor, calibration and any read data is deleted.
+		 */
+		void close();
 
-	int lastReadTime = 0;
+		/*
+		 * Reads the LPG, CO and smoke data from the sensor and returns and
+		 * array with the values in this order.
+		 *
+		 * The read procedure takes `READ_SAMPLE_TIMES` samples from the sensor
+		 * every `READ_SAMPLE_INTERVAL` ms and returns the average.
+		 */
+		float* read(bool print);
+
+		/*
+		 * Same as before but only return the data from the specified gas.
+		 */
+		float readLPG();
+		float readCO();
+		float readSmoke();
+
+	private:
+		int _pin;
+		
+		int RL_VALUE = 5;     //define the load resistance on the board, in kilo ohms
+		int RO_CLEAN_AIR_FACTOR = 9.83;  
+		int CALIBARAION_SAMPLE_TIMES = 5; 
+		int CALIBRATION_SAMPLE_INTERVAL = 50;
+
+		int READ_SAMPLE_TIMES = 5;
+		int READ_SAMPLE_INTERVAL = 50;
+
+		float LPGCurve[3]  =  {2.3,0.21,-0.47}; 
+		float COCurve[3]  =  {2.3,0.72,-0.34};   
+		float SmokeCurve[3] = {2.3,0.53,-0.44};                                                       
+		float Ro = -1.0;
+
+		float values[3];  // array with the measured values in the order: lpg, CO and smoke
+		
+		float MQRead();
+		int MQGetPercentage(float rs_ro_ratio, float *pcurve);
+		float MQCalibration();
+		float MQResistanceCalculation(int raw_adc);
+		bool checkCalibration();
+
+		int lastReadTime = 0;
 };
 
 //#endif
